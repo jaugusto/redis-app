2f4fddb075573b11c9df329af265817a8b3bb2b4
jaugusto <dev@augustolustosa.com>
2022-10-25 21:01:10 -0300

    update checking scripts

---
+++ teste-app.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+RESULT="`wget -q-O- http://localhost:8081`"
+wget -q localhost:8081
+if [$? -eq 0]
+then
+    echo 'ok - service on air!'
+elif [[ $RESULT == *"Number"* ]]
+then
+    echo 'ok - number of visits'
+    echo $RESULT
+else
+    echo 'not ok - number of visits'
+    exit 1
+fi
